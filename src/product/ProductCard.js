c4ad3c2303844e33dcdd0a4ed6d7a6a0a93ffdae
Manthiramoorthy <manthiramoorthyk15@gmail.com>
2025-08-01 08:58:07 +0530

    node version change

--- src/product/ProductCard.js
+++ src/product/ProductCard.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 function ProductCard({ title, description, imageurl }) {
     return (
         <div className="card col-sm-12 col-md-4 col-lg-3 m-4 p-4justify-content-center"  >
-            <img alt="product image" src={imageurl} style={{ height: "200px", width: "200px" }} />
+            <img alt="product" src={imageurl} style={{ height: "200px", width: "200px" }} />
             <h2>{title}</h2>
             <p>{description}</p>
         </div>
